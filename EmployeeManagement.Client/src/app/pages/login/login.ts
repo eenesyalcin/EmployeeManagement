9e524aebf8c007e710718317e2383acf3d21e0c2
Enes YALÇIN <eenes.ylcn@gmail.com>
2026-03-07 06:18:48 +0300

    feat: implement login functionality

--- EmployeeManagement.Client/src/app/pages/login/login.ts
+++ EmployeeManagement.Client/src/app/pages/login/login.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+
+  loginObject: any = {
+    email: '',
+    contactNo: ''
+  }
+
+  http = inject(HttpClient);
+  router = inject(Router);
+
+  onLogin(){
+    this.http.post("http://localhost:5073/api/EmployeeMaster/login", this.loginObject).subscribe({
+      next: (result: any) => {
+        localStorage.setItem('employeeLoginUser', JSON.stringify(result.data));
+        this.router.navigateByUrl("dashboard");
+      },
+      error: (error: any) => {
+        alert(error.error.message);
+      }
+    })
+  }
 
 }
