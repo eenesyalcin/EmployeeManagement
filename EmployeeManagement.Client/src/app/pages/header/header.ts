432417821dfd81083b1650439549622a8c71367f
Enes YALÇIN <eenes.ylcn@gmail.com>
2026-03-08 07:32:07 +0300

    feat: design dashboard

--- EmployeeManagement.Client/src/app/pages/header/header.ts
+++ EmployeeManagement.Client/src/app/pages/header/header.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
+
+  isSidebarClosed = false;
+
+  toggleSidebar() {
+    this.isSidebarClosed = !this.isSidebarClosed;
+  }
+
+  toggleMenu(event: Event): void {
+    const element = (event.currentTarget as HTMLElement).parentElement;
+    if (element) {
+      element.classList.toggle('showMenu');
+    }
+  }
 
 }
