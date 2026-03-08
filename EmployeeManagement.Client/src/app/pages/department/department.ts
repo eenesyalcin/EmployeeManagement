c244ee382499889b3ffa28b01a95b67ac5f5653c
Enes YALÇIN <eenes.ylcn@gmail.com>
2026-03-09 00:02:47 +0300

    feat: design department page

--- EmployeeManagement.Client/src/app/pages/department/department.ts
+++ EmployeeManagement.Client/src/app/pages/department/department.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class Department {
 
+  departments = [
+    { id: 1, name: 'Departman 1', status: 'Aktiflik Durumu 1' },
+    { id: 2, name: 'Departman 2', status: 'Aktiflik Durumu 2' },
+    { id: 3, name: 'Departman 3', status: 'Aktiflik Durumu 3' },
+    { id: 4, name: 'Departman 4', status: 'Aktiflik Durumu 4' },
+    { id: 5, name: 'Departman 5', status: 'Aktiflik Durumu 5' }
+  ];
+
 }
